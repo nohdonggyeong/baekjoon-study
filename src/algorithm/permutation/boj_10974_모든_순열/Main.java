94c709a237fd910116412179fa3908596d13eb75
노동경 <dandy.noh@samsung.com>
2023-06-23 11:14:40 +0900

    모든 순열
    Signed-off-by: 노동경 <dandy.noh@samsung.com>

--- src/algorithm/permutation/boj_10974_모든_순열/Main.java
+++ src/algorithm/permutation/boj_10974_모든_순열/Main.java
@@ -7,18 +7,27 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Main {
-	static int N;
-	static int[] arr;
-	static boolean[] visit;
-	static StringBuilder sb;
+	static int N, R;
+	static int[] arr; // 출력 용도
+	static boolean[] visit; // 확인 용도
+	static StringBuilder sb = new StringBuilder();
 	
 	static void backTracking(int depth) {
-		if (depth == N) {
+		if (depth == R) {
 			for (int val : arr) {
 				sb.append(val).append(" ");
 			}
 			sb.append("\n");
 			return;
+		}
+		
+		for (int i = 0; i < N; i++) {
+			if (!visit[i]) {
+				visit[i] = true;
+				arr[depth] = i + 1;
+				backTracking(depth + 1);
+				visit[i] = false;
+			}
 		}
 	}
 	
@@ -27,9 +36,15 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		N = Integer.parseInt(br.readLine());
+		R = N;
 		
 		arr = new int[N];
 		visit = new boolean[N];
 		
-	}
+		backTracking(0);
+		bw.write(sb.toString());
+		
+		bw.flush();
+		bw.close();
+		br.close();	}
 }
