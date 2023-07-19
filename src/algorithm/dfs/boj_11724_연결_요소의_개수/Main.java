2971c4b9d2ccaf7ea824a06fadf590740aad81b3
MartinNoh <ehdrud1129@naver.com>
2023-07-19 23:14:40 +0900

    dfs 섬의 개수 문제 추가
    Signed-off-by: MartinNoh <ehdrud1129@naver.com>

--- src/algorithm/dfs/boj_11724_연결_요소의_개수/Main.java
+++ src/algorithm/dfs/boj_11724_연결_요소의_개수/Main.java
@@ -5,28 +5,30 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int N, M;
-	static int[][] edges;
-	static boolean[] visited;
+	static List<List<Integer>> graph;
+	static boolean[] visit;
+	static int count;
 	
-	static void dfs(int node) {
-		if (visited[node]) {
-			return;
-		}
-		visited[node] = true;
-		
-		for (int i = 1; i <= N; i++) {
-			if (edges[node][i] == 1) {
-				dfs(i);
+	static void dfs(int vertex) {
+		for(int el : graph.get(vertex)) {
+			if (!visit[el]) {
+				visit[el] = true;
+				dfs(el);
 			}
 		}
 	}
 	
-	public static void main(String[] args) throws IOException {
-		// Input
+	public static void main(String args[]) throws IOException {
+//		LocalDateTime start = LocalDateTime.now();
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
@@ -34,29 +36,38 @@ public class Main {
 		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		edges = new int[N + 1][N + 1];
-		visited = new boolean[N + 1];
+		
+		graph = new ArrayList<>();
+		for (int i = 0; i <= N; i++) {
+			graph.add(new ArrayList<>());
+		}
+		visit = new boolean[N + 1];
 		
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			edges[a][b] = 1;
-			edges[b][a] = 1;
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			
+			graph.get(u).add(v);
+			graph.get(v).add(u);
 		}
-
-		// Process
-		int result = 0;
-		for (int i = 1; i <= N; i++) {
-			if (!visited[i]) {
+		
+		count = 0;
+		for (int i = 1 ; i <= N; i++) {
+			if (!visit[i]) {
+				count++;
+				visit[i] = true;
 				dfs(i);
-				result++;
 			}
 		}
-
-		// Output
-		bw.write(String.valueOf(result));
+		
+		bw.write(String.valueOf(count));
 		bw.flush();
+		
+//		LocalDateTime end = LocalDateTime.now();
+//		System.out.println();
+//		System.out.println("[Elapsed time: " + Duration.between(start, end).getSeconds() + " sec]");
+		
 		bw.close();
 		br.close();
 	}
---
+++ src/algorithm/dfs/boj_11724_연결_요소의_개수/Main_bak.java
@@ -0,0 +1,63 @@
+package algorithm.dfs.boj_11724_연결_요소의_개수;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main_bak {
+	static int N, M;
+	static int[][] edges;
+	static boolean[] visited;
+	
+	static void dfs(int node) {
+		if (visited[node]) {
+			return;
+		}
+		visited[node] = true;
+		
+		for (int i = 1; i <= N; i++) {
+			if (edges[node][i] == 1) {
+				dfs(i);
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		// Input
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		edges = new int[N + 1][N + 1];
+		visited = new boolean[N + 1];
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			edges[a][b] = 1;
+			edges[b][a] = 1;
+		}
+
+		// Process
+		int result = 0;
+		for (int i = 1; i <= N; i++) {
+			if (!visited[i]) {
+				dfs(i);
+				result++;
+			}
+		}
+
+		// Output
+		bw.write(String.valueOf(result));
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
