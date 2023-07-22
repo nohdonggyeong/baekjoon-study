0efd3a87ddbdcc58fee59aa2bf9f776d3b19547e
MartinNoh <ehdrud1129@naver.com>
2023-07-22 14:14:24 +0900

    그릇 만들기 공부 추가
    Signed-off-by: MartinNoh <ehdrud1129@naver.com>

--- src/삼성SW역량테스트/ad_230712_그릇_만들기/Main_bak3.java
+++ src/삼성SW역량테스트/ad_230712_그릇_만들기/Main_bak3.java
@@ -13,10 +13,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main_bak3 {
 	static int T, N, M;
 	static int[] input;
-	static boolean[] visited;
+	static boolean[] visit;
 	static int[] temp;
 	static List<int[]> output;
 	
@@ -24,73 +24,70 @@ public class Main {
 		if (depth == M) {
 			int index = 0;
 			temp = new int[M];
-			for (int i = 0; i < N; i ++) {
-				if (visited[i]) {
+			for (int i = 0; i < N; i++) {
+				if (visit[i]) {
 					temp[index++] = input[i];
 				}
 			}
 			
 			boolean contains = false;
-			for (int[] el : output) {
-				Arrays.sort(temp);
-				Arrays.sort(el);
+			for (int [] el : output) {
 				if (Arrays.equals(el, temp)) {
 					contains = true;
 				}
 			}
 			if (!contains) {
-				output.add(temp.clone());
-				return;	
+				output.add(temp);				
 			}
+			return;
 		}
 		
 		for (int i = start; i < N; i++) {
-			if (!visited[i]) {
-				visited[i] = true;
+			if (!visit[i]) {
+				visit[i] = true;
 				combination(i + 1, depth + 1);
-				visited[i] = false;
+				visit[i] = false;
 			}
 		}
 	}
 	
 	public static void main(String args[]) throws IOException {
-//		LocalDateTime startTime = LocalDateTime.now();
-//		System.setIn(new FileInputStream("src/삼성SW역량테스트/ad_230712_그릇_만들기/input.txt"));
-		
+//		LocalDateTime start = LocalDateTime.now();
+		System.setIn(new FileInputStream("src/삼성SW역량테스트/ad_230712_그릇_만들기/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
+		// test case
 		T = Integer.parseInt(br.readLine());
 		for (int t = 0; t < T; t++) {
+			// setting: N, M, input, visit
 			st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
 			input = new int[N];
+			visit = new boolean[N];
 			
 			st = new StringTokenizer(br.readLine());
-			for (int n = 0; n < N; n++) {
-				input[n] = Integer.parseInt(st.nextToken());
+			for (int i = 0; i < N; i++) {
+				input[i] = Integer.parseInt(st.nextToken());
 			}
 			Arrays.sort(input);
 			
-			visited = new boolean[N];
+			// operation: combination
 			output = new ArrayList<>();
 			combination(0, 0);
 			
-			sb.append("#").append(String.valueOf(t + 1)).append(" ").append(String.valueOf(output.size()));
-			if (t < T - 1) {
-				sb.append("\n");
-			}
+			// print: output.size()
+			sb.append("#").append(String.valueOf(t + 1)).append(" ").append(String.valueOf(output.size())).append("\n");
 		}
 		bw.write(sb.toString());
 		bw.flush();
 		
-//		LocalDateTime endTime = LocalDateTime.now();
+//		LocalDateTime end = LocalDateTime.now();
 //		System.out.println();
-//		System.out.println();
-//		System.out.println("[Elapsed time: " + Duration.between(startTime, endTime).getSeconds() + " sec]");
+//		System.out.println("[Elapsed time: " + Duration.between(start, end).getSeconds() + " sec]");
 		
 		bw.close();
 		br.close();
