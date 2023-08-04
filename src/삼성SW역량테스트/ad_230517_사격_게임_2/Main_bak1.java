4fff74a56ac72ee7fa6dbd0dbbf3ce97de2178a5
노동경 <dandy.noh@samsung.com>
2023-08-04 17:16:48 +0900

    사격 게임2의 Main 파일 변경
    Signed-off-by: 노동경 <dandy.noh@samsung.com>

--- src/삼성SW역량테스트/ad_230517_사격_게임_2/Main_bak1.java
+++ src/삼성SW역량테스트/ad_230517_사격_게임_2/Main_bak1.java
@@ -13,207 +13,222 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main_bak1 {
-	static int T, R, C;
-	static int[][] map, visit;
-	static int combN, combR;
-	static List<int[]> inputCandidateList;
-	static int[] inputCandidateIndex, inputIndexTemp;
-	static List<int[]> inputIndexList;
-	static int answer;
+	static int T, H, W;
+	static int[][] balloons;
+	static int[][] shot;
+	static List<int[]> shootingPositionCandidateList;
+	static int combinationN, combinationR;
+	static int[] shootingPositionCandidateIndex;
+	static int[] combinationTemp;
+	static List<int[]> shootingPositionIndexList;
+	static int maxScore;
 	
-	// 오른쪽으로 직선 사격
-	static void shootRight(int r, int c, boolean clear) {
-		if (!clear) {
-			visit[r][c] += 1;
-			int nc = c + 1;
-			if (nc < C) {
-				shootRight(r, nc, false);
+	static void combinationRepetition(int start, int depth) {
+		if (depth == combinationR) { // 사격 위치 3곳 정했으면 중복 조합 리스트 추가
+			shootingPositionIndexList.add(combinationTemp.clone());
+			return;
+		}
+		
+		for (int i = start; i < combinationN; i++) {
+			combinationTemp[depth] = shootingPositionCandidateIndex[i];
+			combinationRepetition(i, depth + 1);
+		}
+	}
+	
+	// 오른쪽으로 사격
+	static void shootRight(int h, int w, boolean reset) {
+		if (!reset) {
+			shot[h][w] += 1;
+			
+			int newW = w + 1;
+			if (newW < W) {
+				shootRight(h, newW, false);
 			}
 		} else { // 리셋
-			visit[r][c] -= 1;
-			int nc = c + 1;
-			if (nc < C) {
-				shootRight(r, nc, true);
+			shot[h][w] -= 1;
+			
+			int newW = w + 1;
+			if (newW < W) {
+				shootRight(h, newW, true);
 			}
 		}
 	}
 	
 	// 오른쪽 위로 사격
-	static void shootRightUp(int r, int c, boolean clear) {
-		if (!clear) {
-			visit[r][c] += 1;
-			int nr = r - 1;
-			int nc = c + 1;
-			if (nr >= 0 && nc < C) {
-				shootRightUp(nr, nc, false);
+	static void shootRightUp(int h, int w, boolean reset) {
+		if (!reset) {
+			shot[h][w] += 1;
+			
+			int newH = h - 1;
+			int newW = w + 1;
+			if (newH >= 0 && newW < W) {
+				shootRightUp(newH, newW, false);
 			}
 		} else { // 리셋
-			visit[r][c] -= 1;
-			int nr = r - 1;
-			int nc = c + 1;
-			if (nr >= 0 && nc < C) {
-				shootRightUp(nr, nc, true);
+			shot[h][w] -= 1;
+			
+			int newH = h - 1;
+			int newW = w + 1;
+			if (newH >= 0 && newW < W) {
+				shootRightUp(newH, newW, true);
 			}
 		}
 	}
 	
 	// 오른쪽 아래로 사격
-	static void shootRightDown(int r, int c, boolean clear) {
-		if (!clear) {
-			visit[r][c] += 1;
-			int nr = r + 1;
-			int nc = c + 1;
-			if (nr < R && nc < C) {
-				shootRightDown(nr, nc, false);
+	static void shootRightDown(int h, int w, boolean reset) {
+		if (!reset) {
+			shot[h][w] += 1;
+			
+			int newH = h + 1;
+			int newW = w + 1;
+			if (newH < H && newW < W) {
+				shootRightDown(newH, newW, false);
 			}
 		} else { // 리셋
-			visit[r][c] -= 1;
-			int nr = r + 1;
-			int nc = c + 1;
-			if (nr < R && nc < C) {
-				shootRightDown(nr, nc, true);
+			shot[h][w] -= 1;
+			
+			int newH = h + 1;
+			int newW = w + 1;
+			if (newH < H && newW < W) {
+				shootRightDown(newH, newW, true);
 			}
 		}
 	}
 	
-	// 아래로 직선 사격
-	static void shootDown(int r, int c, boolean clear) {
-		if (!clear) {
-			visit[r][c] += 1;
-			int nr = r + 1;
-			if (nr < R) {
-				shootDown(nr, c, false);
+	// 아래로 사격
+	static void shootDown(int h, int w, boolean reset) {
+		if (!reset) {
+			shot[h][w] += 1;
+			
+			int newH = h + 1;
+			if (newH < H) {
+				shootDown(newH, w, false);
 			}
 		} else { // 리셋
-			visit[r][c] -= 1;
-			int nr = r + 1;
-			if (nr < R) {
-				shootDown(nr, c, true);
+			shot[h][w] -= 1;
+			
+			int newH = h + 1;
+			if (newH < H) {
+				shootDown(newH, w, true);
 			}
 		}
 	}
 	
 	// 아래 왼쪽으로 사격
-	static void shootDownLeft(int r, int c, boolean clear) {
-		if (!clear) {
-			visit[r][c] += 1;
-			int nr = r + 1;
-			int nc = c - 1;
-			if (nr < R && nc >= 0) {
-				shootDownLeft(nr, nc, false);
+	static void shootDownLeft(int h, int w, boolean reset) {
+		if (!reset) {
+			shot[h][w] += 1;
+			
+			int newH = h + 1;
+			int newW = w - 1;
+			if (newH < H && newW >= 0) {
+				shootDownLeft(newH, newW, false);
 			}
 		} else { // 리셋
-			visit[r][c] -= 1;
-			int nr = r + 1;
-			int nc = c - 1;
-			if (nr < R && nc >= 0) {
-				shootDownLeft(nr, nc, true);
+			shot[h][w] -= 1;
+			
+			int newH = h + 1;
+			int newW = w - 1;
+			if (newH < H && newW >= 0) {
+				shootDownLeft(newH, newW, true);
 			}
 		}
 	}
 	
-	// 명중된 곳 합계
-	static int getSum() {
-		int sum = 0;
-		for (int r = 0; r < R; r++) {
-			for (int c = 0; c < C; c++) {
-				if (visit[r][c] > 0) {
-					sum += map[r][c];
+	// 게임점수 세기
+	static int getScore() {
+		int score = 0;
+		for (int h = 0; h < H; h++) {
+			for (int w = 0; w < W; w++) {
+				if (shot[h][w] > 0) {
+					score += balloons[h][w];
 				}
 			}
 		}
-		return sum;
+		return score;
 	}
 	
-	// 사격 위치 3곳 중복 조합
-	static void combinationRepetition(int start, int depth) {
-		if (depth == combR) {
-			inputIndexList.add(inputIndexTemp.clone());
-			return;
-		}
-		
-		for (int i = start; i < combN; i++) {
-			inputIndexTemp[depth] = inputCandidateIndex[i];
-			combinationRepetition(i, depth + 1);
-		}
-	}
-	
-	// 사격
-	static void dfs(int depth, int[] inputIndex) {
-		if (depth == combR) {
-//			if (getSum() > answer) {
+	// 사격 경우마다 score 얻기
+	static void shootingDFS(int depth, int[] shootingPositionIndex) {
+		if (depth == 3) {			
+//			if (maxScore < getScore()) {
 //				System.out.println();
-//				for (int r = 0; r < R; r++) {
-//					for (int c = 0; c < C; c++) {
-//						System.out.print(visit[r][c] > 0 ? "O " : "X ");
+//				for (int h = 0; h < H; h++) {
+//					for (int w = 0; w < W; w++) {
+//						System.out.print(shot[h][w] > 0 ? "O " : "X ");
 //					}
 //					System.out.println();
 //				}
+//				System.out.println();
 //			}
 			
-			answer = Math.max(answer, getSum());
+			maxScore = Math.max(maxScore, getScore());
 			return;
 		}
 		
-		// 발사 3번 반복
-		for (int i = 0; i < combR; i ++) {
-			int[] input = inputCandidateList.get(inputIndex[depth]);
-			if (input[0] == 0 && input[1] != 0) { // 발사하는 곳이 (0, c)인 경우
-				switch(i) {
-				case 0:
-					shootDown(0, input[1], false);
-					dfs(depth + 1, inputIndex);
-					shootDown(0, input[1], true);
-					break;
-				case 1:
-					shootDownLeft(0, input[1], false);
-					dfs(depth + 1, inputIndex);
-					shootDownLeft(0, input[1], true);
-					break;
-				case 2:
-					shootRightDown(0,  input[1], false);
-					dfs(depth + 1, inputIndex);
-					shootRightDown(0, input[1], true);
-					break;
-				default:
-					break;
-				}
-			} else if (input[0] != 0 && input[1] == 0) { // 발사하는 곳이 (r, 0)인 경우
-				switch(i) {
-				case 0:
-					shootRight(input[0], 0, false);
-					dfs(depth + 1, inputIndex);
-					shootRight(input[0], 0, true);
-					break;
-				case 1:
-					shootRightUp(input[0], 0, false);
-					dfs(depth + 1, inputIndex);
-					shootRightUp(input[0], 0, true);
-					break;
-				case 2:
-					shootRightDown(input[0], 0, false);
-					dfs(depth + 1, inputIndex);
-					shootRightDown(input[0], 0, true);
-					break;
-				default:
-					break;
-				}
-			} else if (input[0] == 0 && input[0] == 0) { // 발사하는 곳이 (0, 0)인 경우
-				switch(i) {
+		int[] shootingPosition = shootingPositionCandidateList.get(shootingPositionIndex[depth]);
+		if (shootingPosition[0] == 0 && shootingPosition[1] == 0) {
+			for (int i = 0; i < 3; i++) {
+				switch (i) {
 				case 0:
 					shootRight(0, 0, false);
-					dfs(depth + 1, inputIndex);
+					shootingDFS(depth + 1, shootingPositionIndex);
 					shootRight(0, 0, true);
 					break;
 				case 1:
 					shootDown(0, 0, false);
-					dfs(depth + 1, inputIndex);
+					shootingDFS(depth + 1, shootingPositionIndex);
 					shootDown(0, 0, true);
 					break;
 				case 2:
 					shootRightDown(0, 0, false);
-					dfs(depth + 1, inputIndex);
+					shootingDFS(depth + 1, shootingPositionIndex);
 					shootRightDown(0, 0, true);
+					break;
+				default:
+					break;
+				}
+			}
+		} else if (shootingPosition[1] == 0) {
+			for (int i = 0; i < 3; i++) {
+				switch (i) {
+				case 0:
+					shootRight(shootingPosition[0], 0, false);
+					shootingDFS(depth + 1, shootingPositionIndex);
+					shootRight(shootingPosition[0], 0, true);
+					break;
+				case 1:
+					shootRightUp(shootingPosition[0], 0, false);
+					shootingDFS(depth + 1, shootingPositionIndex);
+					shootRightUp(shootingPosition[0], 0, true);
+					break;
+				case 2:
+					shootRightDown(shootingPosition[0], 0, false);
+					shootingDFS(depth + 1, shootingPositionIndex);
+					shootRightDown(shootingPosition[0], 0, true);
+					break;
+				default:
+					break;
+				}
+			}
+		} else if (shootingPosition[0] == 0) {
+			for (int i = 0; i < 3; i++) {
+				switch (i) {
+				case 0:
+					shootDown(0, shootingPosition[1], false);
+					shootingDFS(depth + 1, shootingPositionIndex);
+					shootDown(0, shootingPosition[1], true);
+					break;
+				case 1:
+					shootDownLeft(0, shootingPosition[1], false);
+					shootingDFS(depth + 1, shootingPositionIndex);
+					shootDownLeft(0, shootingPosition[1], true);
+					break;
+				case 2:
+					shootRightDown(0, shootingPosition[1], false);
+					shootingDFS(depth + 1, shootingPositionIndex);
+					shootRightDown(0, shootingPosition[1], true);
 					break;
 				default:
 					break;
@@ -223,66 +238,71 @@ public class Main_bak1 {
 	}
 	
 	public static void main(String args[]) throws IOException {
-//		LocalDateTime start = LocalDateTime.now();
-
-		System.setIn(new FileInputStream("src/삼성SW역량테스트/ad_230517_사격_게임_2/input.txt"));
+//		LocalDateTime startTime = LocalDateTime.now();
+//		System.setIn(new FileInputStream("src/삼성SW역량테스트/ad_230517_사격_게임_2/input.txt"));
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
-		// 테스트케이스
+		// 테스트케이스 반복
 		T = Integer.parseInt(br.readLine());
 		for (int t = 0; t < T; t++) {
-			// map, visit 세팅
+			// H, W, balloons, shot 크기 입력
 			st = new StringTokenizer(br.readLine());
-			R = Integer.parseInt(st.nextToken());
-			C = Integer.parseInt(st.nextToken());
-			map = new int[R][C];
-			visit = new int[R][C];
+			H = Integer.parseInt(st.nextToken());
+			W = Integer.parseInt(st.nextToken());
+			balloons = new int[H][W];
+			shot = new int[H][W];
 			
-			for (int r = 0; r < R; r++) {
+			// 풍선 점수 입력
+			for (int h = 0; h < H; h++) {
 				st = new StringTokenizer(br.readLine());
-				for(int c = 0; c < C; c++) {
-					map[r][c] = Integer.parseInt(st.nextToken());
+				for (int w = 0; w < W; w++) {
+					balloons[h][w] = Integer.parseInt(st.nextToken());
 				}
 			}
 			
-			// input list 세팅
-			inputCandidateList = new ArrayList<>();
-			for (int r = 0; r < R; r++) {
-				inputCandidateList.add(new int[] {r, 0});
+			// 사격 위치 후보 구성
+			shootingPositionCandidateList = new ArrayList<>();
+			for (int h = 0; h < H; h++) {
+				shootingPositionCandidateList.add(new int[] {h, 0});
 			}
-			for (int c = 0; c < C; c++) {
-				inputCandidateList.add(new int[] {0, c});
-			}
-			
-			combN = inputCandidateList.size();
-			combR = 3;
-			inputCandidateIndex = new int[combN];
-			for (int i = 0; i < combN; i++) {
-				inputCandidateIndex[i] = i;
+			for (int w = 1; w < W; w++) { // {0, 0} 중복 제외
+				shootingPositionCandidateList.add(new int[] {0, w});
 			}
 			
-			inputIndexTemp = new int[3];
-			inputIndexList = new ArrayList<>();
+			// 사격 위치 중복 조합
+			combinationN = shootingPositionCandidateList.size();
+			combinationR = 3;
+			shootingPositionCandidateIndex = new int[combinationN];
+			for (int i = 0; i < combinationN; i ++) {
+				shootingPositionCandidateIndex[i] = i;
+			}
+			combinationTemp = new int[combinationR];
+			shootingPositionIndexList = new ArrayList<>();
 			combinationRepetition(0, 0);
 			
-			// 사격 dfs
-			answer = 0;
-			for (int i = 0; i < inputIndexList.size(); i++) {
-				dfs(0, inputIndexList.get(i));
+			// 모든 경우 중 max score 얻기
+			maxScore = 0;
+			for (int[] el : shootingPositionIndexList) {
+				shootingDFS(0, el);
 			}
 			
-			// answer 출력
-			sb.append("#").append(String.valueOf(t + 1)).append(" ").append(String.valueOf(answer)).append("\n");
+			// 테스트케이스 결과 출력
+			sb.append("#").append(String.valueOf(t + 1)).append(" ").append(String.valueOf(maxScore));
+			if (t < T - 1) {
+				sb.append("\n");
+			}
 		}
 		bw.write(sb.toString());
 		bw.flush();
 		
-//		LocalDateTime end = LocalDateTime.now();
+//		LocalDateTime endTime = LocalDateTime.now();
 //		System.out.println();
-//		System.out.println("[Elapsed time: " + Duration.between(start, end).getSeconds() + " sec]");
+//		System.out.println();
+//		System.out.println("[Elapsed time: " + Duration.between(startTime, endTime).getSeconds() + " sec]");
 		
 		bw.close();
 		br.close();
