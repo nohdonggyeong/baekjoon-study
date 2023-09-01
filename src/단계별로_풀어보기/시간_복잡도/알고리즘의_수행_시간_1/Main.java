93b513dc06ec4436e4aa04dafae4caf4b1a86476
Donggyeong <ehdrud1129@naver.com>
2023-09-01 22:48:56 +0900

    백준 단계별로 풀어보기 공부
    Signed-off-by: Donggyeong <ehdrud1129@naver.com>

---
+++ src/단계별로_풀어보기/시간_복잡도/알고리즘의_수행_시간_1/Main.java
@@ -0,0 +1,16 @@
+package 단계별로_풀어보기.시간_복잡도.알고리즘의_수행_시간_1;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String args[]) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write("1");
+		bw.newLine();
+		bw.write("0");
+		bw.flush();
+		bw.close();
+	}
+}
