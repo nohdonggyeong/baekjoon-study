c76f1bdb92b2e00f9cdace7f6f805489e2975704
MartinNoh <ehdrud1129@naver.com>
2023-09-02 01:28:13 +0900

    단계별로 풀어보기 공부 추가
    Signed-off-by: MartinNoh <ehdrud1129@naver.com>

---
+++ src/단계별로_풀어보기/정렬/boj_25305_커트라인/Main.java
@@ -0,0 +1,29 @@
+package 단계별로_풀어보기.정렬.boj_25305_커트라인;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		bw.write(String.valueOf(arr[n-k]));
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
