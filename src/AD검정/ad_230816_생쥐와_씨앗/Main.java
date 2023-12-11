891ca11320c1ae0d1e201d9609b6005f4579936a
노동경 <dandy.noh@samsung.com>
2023-12-11 17:47:50 +0900

    생쥐와 씨앗 문제 추가
    Signed-off-by: 노동경 <dandy.noh@samsung.com>

--- src/AD검정/ad_230816_생쥐와_씨앗/Main.java
+++ src/AD검정/ad_230816_생쥐와_씨앗/Main.java
@@ -5,108 +5,144 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Queue;
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int T, N, result;
-	static Queue<int[]> seedQueue;
-	static int[] mouse, cat;
-	static boolean breakFlag;
+	// 다음 씨앗 목표 큐
+	static Queue<Node> seedQueue;
+	// 쥐, 고양이의 위치
+	static Node mouse, cat;
 	
-	static void chase(int[] nextDestination) {
-		while (true) {
-			// 생쥐가 1의 이동거리로 먼저 이동
-			// 생쥐는 좌우 우선 최단으로 이동
-			if (mouse[0] != nextDestination[0]) {
-				mouse[0] += nextDestination[0] - mouse[0] > 0 ? 1 : -1;
-			} else {
-				mouse[1] += nextDestination[1] - mouse[1] > 0 ? 1 : -1;
+	static class Node{
+		private int x;
+		private int y;
+		
+		public Node() {
+			this.x = 0;
+			this.y = 0;
+		}
+		
+		public Node(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	
+	static boolean solution() {
+		Node nextSeed = null;
+		while (true) {			
+			if (nextSeed == null) {
+				if (mouse.x == 1 && mouse.y == 1) {
+					return true;
+				}
+				
+				if (seedQueue.isEmpty()) {
+					return true;
+				}
+				
+				nextSeed = seedQueue.poll();				
 			}
 			
-			// 생쥐가 고양이에게 잡혔나 확인
-			if (mouse[0] == cat[0] && mouse[1] == cat[1]) {				
-				breakFlag = true;
-				result = 0;
-				return;
-			}
-			
-			// 고양이가 1 또는 2의 이동거리로 이동
-			// 고양이는 상하 우선 최단으로 이동
-			if (Math.abs(cat[1] - mouse[1]) >= 2) {
-				cat[1] += mouse[1] - cat[1] > 0 ? 2 : -2;
-			} else if (Math.abs(cat[1] - mouse[1]) == 1) {
-				if (Math.abs(cat[0] - mouse[0]) >= 2) {
-					cat[0] += mouse[0] - cat[0] > 0 ? 2 : -2;	
+			// 생쥐와 다음 씨앗 사이 가로 및 세로 길이 비교하고 생쥐 이동
+			if (mouse.x - nextSeed.x == 0) {
+				if (mouse.y > nextSeed.y) {
+					mouse.y -= 1;
 				} else {
-					cat[1] += mouse[1] - cat[1] > 0 ? 1 : -1;
+					mouse.y += 1;
 				}
-			} else if (Math.abs(cat[1] - mouse[1]) == 0) {
-				if (Math.abs(cat[0] - mouse[0]) >= 2) {
-					cat[0] += mouse[0] - cat[0] > 0 ? 2 : -2;	
-				} else if (Math.abs(cat[0] - mouse[0]) == 1) {
-					cat[0] += mouse[0] - cat[0] > 0 ? 1 : -1;
+			} else { // 쥐는 좌우우선
+				if (mouse.x > nextSeed.x) {
+					mouse.x -= 1;
+				} else {
+					mouse.x += 1;
+				}
+			}
+
+			// 판단
+			if (cat.x == mouse.x && cat.y == mouse.y) {
+				return false;
+			}
+			
+			// 고양이와 생쥐 사이 가로 및 세로 길이 비교하고 고양이 이동
+			if (Math.abs(cat.y - mouse.y) >= 2) {
+				// 상하 2 이동
+				if (cat.y > mouse.y) {
+					cat.y -= 2;
+				} else {
+					cat.y += 2;
+				}
+			} else if (Math.abs(cat.y - mouse.y) == 1) {
+				// x가 2보다 크거나 같으면 좌우 이동
+				// 그렇지 않으면 상하 1 이동
+				if (Math.abs(cat.x - mouse.x) >= 2) {
+					if (cat.x > mouse.x) {
+						cat.x -= 2;
+					} else {
+						cat.x += 2;
+					}
+				} else {
+					if (cat.y > mouse.y) {
+						cat.y -= 1;
+					} else {
+						cat.y += 1;
+					}
+				}
+			} else {
+				// 쥐와 고양이가 y값이 같다면 좌우 이동
+				if (Math.abs(cat.x - mouse.x) >= 2) {
+					if (cat.x > mouse.x) {
+						cat.x -= 2;
+					} else {
+						cat.x += 2;
+					}
+				} else {
+					if (cat.x >= mouse.x) {
+						cat.x -= 1;
+					} else {
+						cat.x += 1;
+					}
 				}
 			}
 			
-			// 고양이가 생쥐를 잡았나 확인
-			if (mouse[0] == cat[0] && mouse[1] == cat[1]) {				
-				breakFlag = true;
-				result = 0;
-				return;
+			// 판단
+			if (cat.x == mouse.x && cat.y == mouse.y) {
+				return false;
 			}
 			
-			// 생쥐가 굴 앞인 1,1에 도착했나 확인
-			// 굴 앞으로 도착했다면 생쥐 탈출 성공
-			if (mouse[0] == 0 && mouse[1] == 1) {
-				breakFlag = true;
-				return;
-			}
-			
-			// 생쥐가 다음 목적지를 향해야 하나 확인
-			if (mouse[0] == nextDestination[0] && mouse[1] == nextDestination[1]) {
-				return;
+			// 다음 씨앗을 목표로 함
+			if (mouse.x == nextSeed.x && mouse.y == nextSeed.y) {
+				nextSeed = null;
 			}
 		}
 	}
 	
-	public static void main(String args[]) throws IOException{
+	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-		
-		T = Integer.parseInt(br.readLine());
-		for (int t = 1; t <= T; t++) {
-			// 주어지는 씨앗의 개수
-			N = Integer.parseInt(br.readLine());
-			
-			// 주어지는 씨앗의 위치 x, y
+		StringBuilder sb = new StringBuilder();
+				
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 0 ; t < T; t++) {
 			seedQueue = new LinkedList<>();
-			for (int i = 0; i < N; i++) {
+			int N = Integer.parseInt(br.readLine());
+			for (int n = 0; n < N; n++) {
 				st = new StringTokenizer(br.readLine());
-				seedQueue.offer(new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
+				seedQueue.offer(new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
 			}
-			// x == 1, y == 1의 가장자리를 벗어나지 않기 위해 생쥐 구멍으로 지나가야 하는 1, 1을 목적지로 추가
-			// 생쥐의 이동거리는 1이기 때문에 생쥐가 1,1에 도착하고도 고양이에게 먹히는지 판단하면 될 것
-			seedQueue.offer(new int[] {1, 1});
+			seedQueue.offer(new Node(1, 1)); // 쥐굴
 			
-			// 주어지는 고양이 시작 위치
 			st = new StringTokenizer(br.readLine());
-			cat = new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+			cat = new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+			mouse = seedQueue.poll();
 			
-			// 쥐가 입력받은 씨앗 위치 순서대로 생쥐 구멍까지 이동하면서
-			// 고양이에게 잡히나 안잡히나 확인
-			result = 1;
-			mouse = seedQueue.poll(); // 생쥐는 씨앗의 첫 입력 위치부터 시작
-			breakFlag = false;
+			// solution
+			boolean result = solution();
 			
-			while (!breakFlag && !seedQueue.isEmpty()) {
-				chase(seedQueue.poll());
-			}
-			
-			sb.append("#").append(t).append(" ").append(result).append("\n");	
+			// print result
+			sb.append("#").append(String.valueOf(t + 1)).append(" ").append(result ? "1" : "0").append("\n");
 		}
 		bw.write(sb.toString());
 		bw.flush();
---
+++ src/AD검정/ad_230816_생쥐와_씨앗/Main3.java
@@ -0,0 +1,116 @@
+package AD검정.ad_230816_생쥐와_씨앗;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main3 {
+	static int T, N, result;
+	static Queue<int[]> seedQueue;
+	static int[] mouse, cat;
+	static boolean breakFlag;
+	
+	static void chase(int[] nextDestination) {
+		while (true) {
+			// 생쥐가 1의 이동거리로 먼저 이동
+			// 생쥐는 좌우 우선 최단으로 이동
+			if (mouse[0] != nextDestination[0]) {
+				mouse[0] += nextDestination[0] - mouse[0] > 0 ? 1 : -1;
+			} else {
+				mouse[1] += nextDestination[1] - mouse[1] > 0 ? 1 : -1;
+			}
+			
+			// 생쥐가 고양이에게 잡혔나 확인
+			if (mouse[0] == cat[0] && mouse[1] == cat[1]) {				
+				breakFlag = true;
+				result = 0;
+				return;
+			}
+			
+			// 고양이가 1 또는 2의 이동거리로 이동
+			// 고양이는 상하 우선 최단으로 이동
+			if (Math.abs(cat[1] - mouse[1]) >= 2) {
+				cat[1] += mouse[1] - cat[1] > 0 ? 2 : -2;
+			} else if (Math.abs(cat[1] - mouse[1]) == 1) {
+				if (Math.abs(cat[0] - mouse[0]) >= 2) {
+					cat[0] += mouse[0] - cat[0] > 0 ? 2 : -2;	
+				} else {
+					cat[1] += mouse[1] - cat[1] > 0 ? 1 : -1;
+				}
+			} else if (Math.abs(cat[1] - mouse[1]) == 0) {
+				if (Math.abs(cat[0] - mouse[0]) >= 2) {
+					cat[0] += mouse[0] - cat[0] > 0 ? 2 : -2;	
+				} else if (Math.abs(cat[0] - mouse[0]) == 1) {
+					cat[0] += mouse[0] - cat[0] > 0 ? 1 : -1;
+				}
+			}
+			
+			// 고양이가 생쥐를 잡았나 확인
+			if (mouse[0] == cat[0] && mouse[1] == cat[1]) {				
+				breakFlag = true;
+				result = 0;
+				return;
+			}
+			
+			// 생쥐가 굴 앞인 1,1에 도착했나 확인
+			// 굴 앞으로 도착했다면 생쥐 탈출 성공
+			if (mouse[0] == 0 && mouse[1] == 1) {
+				breakFlag = true;
+				return;
+			}
+			
+			// 생쥐가 다음 목적지를 향해야 하나 확인
+			if (mouse[0] == nextDestination[0] && mouse[1] == nextDestination[1]) {
+				return;
+			}
+		}
+	}
+	
+	public static void main(String args[]) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		T = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++) {
+			// 주어지는 씨앗의 개수
+			N = Integer.parseInt(br.readLine());
+			
+			// 주어지는 씨앗의 위치 x, y
+			seedQueue = new LinkedList<>();
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				seedQueue.offer(new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
+			}
+			// x == 1, y == 1의 가장자리를 벗어나지 않기 위해 생쥐 구멍으로 지나가야 하는 1, 1을 목적지로 추가
+			// 생쥐의 이동거리는 1이기 때문에 생쥐가 1,1에 도착하고도 고양이에게 먹히는지 판단하면 될 것
+			seedQueue.offer(new int[] {1, 1});
+			
+			// 주어지는 고양이 시작 위치
+			st = new StringTokenizer(br.readLine());
+			cat = new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+			
+			// 쥐가 입력받은 씨앗 위치 순서대로 생쥐 구멍까지 이동하면서
+			// 고양이에게 잡히나 안잡히나 확인
+			result = 1;
+			mouse = seedQueue.poll(); // 생쥐는 씨앗의 첫 입력 위치부터 시작
+			breakFlag = false;
+			
+			while (!breakFlag && !seedQueue.isEmpty()) {
+				chase(seedQueue.poll());
+			}
+			
+			sb.append("#").append(t).append(" ").append(result).append("\n");	
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
