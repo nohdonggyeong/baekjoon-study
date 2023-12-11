e6d6676ac0517642cc089087fa352d159284fab2
Noh Donggyeong <ehdrud1129@naver.com>
2023-12-11 22:37:07 +0900

    permutation with repetition 공부
    Signed-off-by: Noh Donggyeong <ehdrud1129@naver.com>

--- src/알고리즘/순열/template_permutation_repetition/Main_bak16.java
+++ src/알고리즘/순열/template_permutation_repetition/Main_bak16.java
@@ -2,47 +2,54 @@ package 알고리즘.순열.template_permutation_repetition;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.StringTokenizer;
 
-public class Main {
+public class Main_bak16 {
 	static int n, r;
 	static int[] input;
 	static int[] temp;
 	static List<int[]> output;
 	
-	static void permutationWithRepetition(int depth) {
+	static void permutationRepetition(int depth) {
 		if (depth == r) {
 			output.add(temp.clone());
 			return;
 		}
 		
-		for (int i = 0; i < n; i++) {
+		for (int i = 0 ; i < n; i++) {
 			temp[depth] = input[i];
-			permutationWithRepetition(depth + 1);
+			permutationRepetition(depth + 1);
 		}
 	}
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String args[]) throws IOException {
+		System.setIn(new FileInputStream("src\\알고리즘\\순열\\input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 		
-		n = 10;
-		r = 3;
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		
 		input = new int[n];
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n; i++) {
-			input[i] = i + 1;
+			input[i] = Integer.parseInt(st.nextToken());
 		}
 		Arrays.sort(input);
 		
 		temp = new int[r];
-		output = new ArrayList<int[]>();
-		permutationWithRepetition(0);
+		output = new ArrayList<>();
+		permutationRepetition(0);
 		
 		for (int[] el : output) {
 			for (int e : el) {
@@ -55,5 +62,4 @@ public class Main {
 		bw.close();
 		br.close();
 	}
-
 }
