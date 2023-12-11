182979f79118a17c48220657cefe7a993e4d2b3b
Noh Donggyeong <ehdrud1129@naver.com>
2023-12-11 22:58:28 +0900

    combination with repetition 공부
    Signed-off-by: Noh Donggyeong <ehdrud1129@naver.com>

--- src/알고리즘/순열/template_combination_repetition/Main_bak16.java
+++ src/알고리즘/순열/template_combination_repetition/Main_bak16.java
@@ -2,19 +2,22 @@ package 알고리즘.순열.template_combination_repetition;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.StringTokenizer;
 
-public class Main {
+public class Main_bak16 {
 	static int n, r;
-	static int[] input, temp;
+	static int[] input;
+	static int[] temp;
 	static List<int[]> output;
 	
-	static void combinationWithRepetition(int start, int depth) {
+	static void combinationRepetition(int start, int depth) {
 		if (depth == r) {
 			output.add(temp.clone());
 			return;
@@ -22,25 +25,31 @@ public class Main {
 		
 		for (int i = start; i < n; i++) {
 			temp[depth] = input[i];
-			combinationWithRepetition(i, depth + 1);
+			combinationRepetition(i, depth + 1);
 		}
 	}
-	public static void main(String[] args) throws IOException {
+	
+	public static void main(String args[]) throws IOException {
+		System.setIn(new FileInputStream("src\\알고리즘\\순열\\input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 		
-		n = 10;
-		r = 3;
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		
 		input = new int[n];
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n; i++) {
-			input[i] = i + 1;
+			input[i] = Integer.parseInt(st.nextToken());
 		}
 		Arrays.sort(input);
 		
 		temp = new int[r];
-		output = new ArrayList<int[]>();
-		combinationWithRepetition(0, 0);
+		output = new ArrayList<>();
+		combinationRepetition(0, 0);
 		
 		for (int[] el : output) {
 			for (int e : el) {
