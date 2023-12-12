7d024dc83aee299502ce12d8012868acab2bf11e
노동경 <dandy.noh@samsung.com>
2023-12-12 10:31:45 +0900

    일일 조합 공부 추가
    Signed-off-by: 노동경 <dandy.noh@samsung.com>

--- src/알고리즘/순열/template_combination/Main_bak17.java
+++ src/알고리즘/순열/template_combination/Main_bak17.java
@@ -7,19 +7,21 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-public class Main {
-	static int N, R;
-	static int[] input, temp;
+public class Main_bak17 {
+	static int n, r;
+	static Integer[] input;
 	static boolean[] visited;
+	static int[] temp;
 	static List<int[]> output;
 	
 	static void combination(int start, int depth) {
-		if (depth == R) {
+		if (depth == r) {
 			int index = 0;
-			temp = new int[R];
-			for (int i = 0; i < N; i++) {
+			temp = new int[r];
+			for (int i = 0; i < n; i++) {
 				if (visited[i]) {
 					temp[index++] = input[i];
 				}
@@ -28,7 +30,7 @@ public class Main {
 			return;
 		}
 		
-		for (int i = start; i < N; i++) {
+		for (int i = start; i < n; i++) {
 			if (!visited[i]) {
 				visited[i] = true;
 				combination(i + 1, depth + 1);
@@ -36,30 +38,30 @@ public class Main {
 			}
 		}
 	}
-	
-	public static void main(String[] args) throws IOException {
+	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
 		
-		N = 10;
-		R = 4;
-		input = new int[N];
-		for (int i = 0; i < N; i++) {
+		n = 10;
+		r = 3;
+		input = new Integer[n];
+		for (int i = 0; i < n; i++) {
 			input[i] = i + 1;
 		}
-		Arrays.sort(input);
+//		Arrays.sort(input, Collections.reverseOrder());
 //		for (int el : input) {
-//			System.out.print(el + " ");
+//			System.out.print(String.valueOf(el) + " ");
 //		}
 		
-		visited = new boolean[N];
-		output = new ArrayList<>();
+		visited = new boolean[n];
+		temp = new int[r];
+		output = new ArrayList<int[]>();
 		combination(0, 0);
 		
 		for (int[] el : output) {
 			for (int e : el) {
-				sb.append(e).append(" ");
+				sb.append(String.valueOf(e)).append(" ");
 			}
 			sb.append("\n");
 		}
@@ -68,5 +70,4 @@ public class Main {
 		bw.close();
 		br.close();
 	}
-
 }
