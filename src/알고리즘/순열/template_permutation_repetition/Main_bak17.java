50d9d61c1c05dc528bf456fd54f9405ce568f4e5
노동경 <dandy.noh@samsung.com>
2023-12-12 10:17:09 +0900

    일일 중복순열 공부 추가
    Signed-off-by: 노동경 <dandy.noh@samsung.com>

--- src/알고리즘/순열/template_permutation_repetition/Main_bak17.java
+++ src/알고리즘/순열/template_permutation_repetition/Main_bak17.java
@@ -7,22 +7,21 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
-public class Main {
-	static int N, R;
-	static Integer[] input;
+public class Main_bak17 {
+	static int n, r;
+	static int[] input;
 	static int[] temp;
 	static List<int[]> output;
 	
 	static void permutationWithRepetition(int depth) {
-		if (depth == R) {
+		if (depth == r) {
 			output.add(temp.clone());
 			return;
 		}
 		
-		for (int i = 0; i < N; i++) {
+		for (int i = 0; i < n; i++) {
 			temp[depth] = input[i];
 			permutationWithRepetition(depth + 1);
 		}
@@ -33,20 +32,16 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
 		
-		N = 10;
-		R = 4;
-		
-		input = new Integer[N];
-		for (int i = 0; i < N; i++) {
+		n = 10;
+		r = 3;
+		input = new int[n];
+		for (int i = 0; i < n; i++) {
 			input[i] = i + 1;
 		}
-		Arrays.sort(input, Collections.reverseOrder());
-//		for (int el : input) {
-//			System.out.print(el + " ");
-//		}
+		Arrays.sort(input);
 		
-		temp = new int[R];
-		output = new ArrayList<>();
+		temp = new int[r];
+		output = new ArrayList<int[]>();
 		permutationWithRepetition(0);
 		
 		for (int[] el : output) {
@@ -57,6 +52,7 @@ public class Main {
 		}
 		bw.write(sb.toString());
 		bw.flush();
+		bw.close();
 		br.close();
 	}
 
