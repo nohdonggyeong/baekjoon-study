2b5df1c0464691c264ba94520573e9749e9f9a67
Noh Donggyeong <ehdrud1129@naver.com>
2023-12-13 18:50:34 +0900

    패키지명 변경
    Signed-off-by: Noh Donggyeong <ehdrud1129@naver.com>

---
+++ src/알고리즘/순열/template_combination_with_repetition/Main_bak02.java
@@ -0,0 +1,74 @@
+package 알고리즘.순열.template_combination_with_repetition;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main_bak02 {
+	static int n, r;
+	static int[] input;
+	static int[] temp;
+	static List<int[]> output;
+	
+	static void combinationDuplicate(int start, int depth) {
+		if (depth == r) {
+			output.add(temp.clone());
+			return;
+		}
+		
+		for (int i = start; i < n; i++) {
+			temp[depth] = input[i];
+			combinationDuplicate(i, depth +1);
+		}
+	}
+	
+	public static void main(String arg[]) throws IOException {
+		LocalDateTime start = LocalDateTime.now();
+		
+		System.setIn(new FileInputStream("src/algorithm/permutation/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		input = new int[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(input);
+		
+		temp = new int[r];
+		output = new ArrayList<>();
+		combinationDuplicate(0, 0);
+		
+		for (int[] el : output) {
+			for (int e : el) {
+				sb.append(String.valueOf(e)).append(" ");
+			}
+			sb.append("\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		
+		LocalDateTime end = LocalDateTime.now();
+		System.out.println();
+		System.out.println(Duration.between(start, end).getSeconds());
+		
+		bw.close();
+		br.close();
+	}
+}
---
+++ src/알고리즘/순열/template_combination_with_repetition/Main_bak09.java
@@ -0,0 +1,67 @@
+package 알고리즘.순열.template_combination_with_repetition;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main_bak09 {
+	static int n, r;
+	static Integer[] input;
+	static int[] temp;
+	static List<int[]> output;
+	
+	static void combinationRepetition(int start, int depth) {
+		if (depth == r) {
+			output.add(temp.clone());
+			return;
+		}
+		
+		for (int i = start; i < n; i++) {
+			temp[depth] = input[i];
+			combinationRepetition(i, depth + 1);
+		}
+	}
+	
+	public void main(String args[]) throws IOException {
+		System.setIn(new FileInputStream("src/algorithm/permutation/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		input = new Integer[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(input, Collections.reverseOrder());
+		
+		temp = new int[r];
+		output = new ArrayList<>();
+		combinationRepetition(0, 0);
+		
+		for (int[] el : output) {
+			for (int e : el) {
+				sb.append(String.valueOf(e)).append(" ");
+			}
+			sb.append("\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		
+		bw.close();
+		br.close();
+	}
+}
