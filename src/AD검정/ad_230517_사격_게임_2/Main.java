4893a390353708439d7c160ea981a559bcfaa443
노동경 <dandy.noh@samsung.com>
2023-12-14 22:14:09 +0900

    사격 게임
    Signed-off-by: 노동경 <dandy.noh@samsung.com>

--- src/AD검정/ad_230517_사격_게임_2/Main.java
+++ src/AD검정/ad_230517_사격_게임_2/Main.java
@@ -20,22 +20,124 @@ public class Main {
 	static List<Node> shootingList;
 	
 	static class Node {
-		private int x;
-		private int y;
+		private int h;
+		private int w;
 		
-		Node(int x, int y) {
-			this.x = x;
-			this.y = y;
+		Node(int h, int w) {
+			this.h = h;
+			this.w = w;
 		}
 	}
 	
 	static int n, r;
 	static int[] input, temp;
-	static boolean[] visited;
 	static List<int[]> output;
 	
+	static void combinationWithRepetition(int start, int depth) {
+		if (depth == r) {
+			output.add(temp.clone());
+			return;
+		}
+		
+		for (int i = start; i < n; i++) {
+			temp[depth] = input[i];
+			combinationWithRepetition(i, depth + 1);
+		}
+	}
+	
+	static int[][] shotCount;
+	
+	static void shootRightUp(Node node, Boolean rollBack) {
+		if (node.h < 0 || node.w >= W) {
+			return;
+		}
+		if (!rollBack) {
+			shotCount[node.h][node.w] += 1;
+		} else {
+			shotCount[node.h][node.w] -= 1;
+		}
+		shootRightUp(new Node(node.h - 1, node.w + 1), rollBack);
+	}
+	
+	static void shootRightStraight(Node node, Boolean rollBack) {
+		if (node.w >= W) {
+			return;
+		}
+		if (!rollBack) {
+			shotCount[node.h][node.w] += 1;
+		} else {
+			shotCount[node.h][node.w] -= 1;
+		}
+		shootRightUp(new Node(node.h, node.w + 1), rollBack);
+	}
+	
+	static void shootRightDown(Node node, Boolean rollBack) {
+		if (node.h >= H || node.w >= W) {
+			return;
+		}
+		if (!rollBack) {
+			shotCount[node.h][node.w] += 1;
+		} else {
+			shotCount[node.h][node.w] -= 1;
+		}
+		shootRightUp(new Node(node.h + 1, node.w + 1), rollBack);
+	}
+	
+	static void shootDownLeft(Node node, Boolean rollBack) {
+		if (node.h >= H || node.w < 0) {
+			return;
+		}
+		if (!rollBack) {
+			shotCount[node.h][node.w] += 1;
+		} else {
+			shotCount[node.h][node.w] -= 1;
+		}
+		shootDownLeft(new Node(node.h + 1, node.w - 1), rollBack);
+	}
+	
+	static void shootDownStraight(Node node, Boolean rollBack) {
+		if (node.h >= H) {
+			return;
+		}
+		if (!rollBack) {
+			shotCount[node.h][node.w] += 1;
+		} else {
+			shotCount[node.h][node.w] -= 1;
+		}
+		shootDownStraight(new Node(node.h + 1, node.w), rollBack);
+	}
+	
+	static void shootDownRight(Node node, Boolean rollBack) {
+		if (node.h >= H || node.w >= W) {
+			return;
+		}
+		if (!rollBack) {
+			shotCount[node.h][node.w] += 1;
+		} else {
+			shotCount[node.h][node.w] -= 1;
+		}
+		shootDownRight(new Node(node.h + 1, node.w + 1), rollBack);
+	}
+	
+	static void dfs(int[] el) {
+		
+	}
+	
+	static int gamePoint;
+	
+	static int getGamePoint() {
+		for (int h = 0; h < H; h++) {
+			for (int w = 0; w < W; w++) {
+				if (shotCount[h][w] > 0) {
+					gamePoint += D[h][w];
+				}
+			}
+		}
+		return gamePoint;
+	}
+	
 	static void solution() {
-		// 사격장소 리스트
+		// 사격장소 리스트 구성
 		shootingList = new ArrayList<>();
 		for (int h = 0; h < H; h++) {
 			shootingList.add(new Node(h, 0));
@@ -50,6 +152,20 @@ public class Main {
 		input = new int[H + W - 1];
 		for (int i = 0; i < H + W - 1; i++) {
 			input[i] = i + 1;
+		}
+		
+		temp = new int[r];
+		output = new ArrayList<>();
+		combinationWithRepetition(0, 0);
+		
+		// 경우마다 계산
+		for (int[] el : output) {
+			shotCount = new int[H][W];
+			dfs(el);
+			
+			gamePoint = 0;
+			getGamePoint();
+			result = Math.max(result, gamePoint);
 		}
 	}
 	
