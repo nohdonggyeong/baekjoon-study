341411d282e1c292d46e5b910be11d4eda504a9e
MartinNoh <ehdrud1129@naver.com>
2024-01-25 02:26:12 +0900

    세그먼트 트리 공부 추가
    Signed-off-by: MartinNoh <ehdrud1129@naver.com>

--- src/알고리즘/세그먼트트리/boj_2042_구간_합_구하기/Main_v2.java
+++ src/알고리즘/세그먼트트리/boj_2042_구간_합_구하기/Main_v2.java
@@ -6,19 +6,18 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main_v2 {
 	static int N, M, K;
 	static long[] tree;
-	static int cacheEnd;
 	
-	static void init(int index) {
+	private static void setCache(int index) {
 		while (index > 1) {
 			tree[index / 2] += tree[index];
-			index--;	
+			index--;
 		}
 	}
 	
-	static void update(int index, long number) {
+	private static void updateNumber(int index, long number) {
 		long diff = number - tree[index];
 		while (index > 0) {
 			tree[index] += diff;
@@ -26,21 +25,21 @@ public class Main {
 		}
 	}
 	
-	static long sum(int start, int end) {
-		long sum = 0;
+	private static long getSum(int start, int end) {
+		long partSum = 0;
 		while (start <= end) {
 			if (start % 2 == 1) {
-				sum += tree[start];
+				partSum += tree[start];
 				start++;
 			}
 			if (end % 2 == 0) {
-				sum += tree[end];
+				partSum += tree[end];
 				end--;
 			}
 			start /= 2;
 			end /= 2;
 		}
-		return sum;
+		return partSum;
 	}
 	
 	public static void main(String[] args) {
@@ -57,22 +56,21 @@ public class Main {
 			int height = (int) Math.ceil(Math.log(N) / Math.log(2));
 			int size = (int) Math.pow(2, height + 1) - 1;
 			tree = new long[size + 1];
-			cacheEnd = size / 2;
-			for (int n = 1; n <= N; n++) {
-				tree[cacheEnd + n] = Long.parseLong(br.readLine());
+			int cacheEnd = size / 2;
+			for (int i = cacheEnd + 1; i <= cacheEnd + N; i++) {
+				tree[i] = Long.parseLong(br.readLine());
 			}
-			init(size);
+			setCache(size);
 			
 			for (int i = 0; i < M + K; i++) {
 				st = new StringTokenizer(br.readLine());
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
 				long c = Long.parseLong(st.nextToken());
-				
 				if (a == 1) {
-					update(cacheEnd + b, c);
+					updateNumber(cacheEnd + b, c);
 				} else if (a == 2) {
-					sb.append(sum(cacheEnd + b, cacheEnd + (int) c)).append("\n");
+					sb.append(getSum(cacheEnd + b, cacheEnd + (int) c)).append("\n");
 				}
 			}
 			
