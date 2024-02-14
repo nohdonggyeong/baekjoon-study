90d23b2ebdcb93d23f1337c0052b69f9ecd4e5f9
노동경 <dandy.noh@samsung.com>
2024-02-14 16:45:03 +0900

    도로 네트워크 공부 중
    Signed-off-by: 노동경 <dandy.noh@samsung.com>

---
+++ src/알고리즘/최소공통조상/boj_3176_도로_네트워크/Main.java
@@ -0,0 +1,149 @@
+package 알고리즘.최소공통조상.boj_3176_도로_네트워크;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, K;
+	static List<Node>[] adjList;
+	static int kMax;
+	static int[][] parent;
+	static int[] depth;
+	
+	static void bfs(int root) {
+		Queue<Integer> queue = new LinkedList<Integer>();
+		queue.add(root);
+		
+		boolean[] visited = new boolean[N + 1];
+		visited[root] = true;
+		
+		int nextLevel = 1;
+		int count = 0;
+		int nowSize = 1;
+		while (!queue.isEmpty()) {
+			int now = queue.remove();
+			for (Node nextNode : adjList[now]) {
+				if (!visited[nextNode.end]) {
+					visited[nextNode.end] = true;
+					queue.add(nextNode.end);
+					
+					parent[0][nextNode.end] = now;
+					depth[nextNode.end] = nextLevel;
+				}
+			}
+			count++;
+			
+			if (count == nowSize) {
+				nextLevel++;
+				count = 0;
+				nowSize = queue.size();
+			}
+		}
+	}
+	
+	static int lca(int a, int b) {
+		if (depth[a] < depth[b]) {
+			int temp = a;
+			a = b;
+			b = temp;
+		}
+		
+		for (int k = kMax; k >= 0; k--) {
+			if (Math.pow(2, k) <= depth[a] - depth[b]) {
+				if (depth[b] <= depth[parent[k][a]]) {
+					a = parent[k][a];
+				}
+			}
+		}
+		
+		for (int k = kMax; k >= 0; k--) {
+			if (a != b) {
+				if (parent[k][a] != parent[k][b]) {
+					a = parent[k][a];
+					b = parent[k][b];
+				}
+			}
+		}
+		
+		int result = a;
+		if (a != b) {
+			result = parent[0][result];
+		}
+		
+		return result;
+	}
+	
+	static class Node {
+		int end;
+		int weight;
+		
+		Node (int end, int weight) {
+			this.end = end;
+			this.weight = weight;
+		}
+	}
+	
+	public static void main(String[] args) {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+			StringTokenizer st;
+			StringBuilder sb = new StringBuilder();
+			
+			N = Integer.parseInt(br.readLine());
+			
+			adjList = new ArrayList[N + 1];
+			for (int n = 1; n <= N; n++) {
+				adjList[n] = new ArrayList<>();
+			}
+			
+			int A, B, C;
+			for (int n = 1; n < N; n++) {
+				st = new StringTokenizer(br.readLine());
+				A = Integer.parseInt(st.nextToken());
+				B = Integer.parseInt(st.nextToken());
+				C = Integer.parseInt(st.nextToken());
+				
+				adjList[A].add(new Node(B, C));
+				adjList[B].add(new Node(A, C));
+			}
+			
+			kMax = 0;
+			int temp = 1;
+			while (temp <= N) {
+				temp *= 2;
+				kMax++;
+			}
+			
+			parent = new int[kMax + 1][N + 1];
+			depth = new int[N + 1];
+			bfs(1);
+			for (int k = 1; k <= kMax; k++) {
+				for (int n = 1; n <= N; n++) {
+					parent[k][n] = parent[k - 1][parent[k - 1][n]];
+				}
+			}
+			
+			K = Integer.parseInt(br.readLine());
+			int D, E, lca, distance;
+			int minDist = Integer.MAX_VALUE;
+			int maxDist = Integer.MIN_VALUE; 
+			for (int k = 0; k < K; k++) {
+				st = new StringTokenizer(br.readLine());
+				D = Integer.parseInt(st.nextToken());
+				E = Integer.parseInt(st.nextToken());
+				
+				lca = lca(D, E);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
