607004fab180701fd40994b601a565a73369b2d3
MartinNoh <ehdrud1129@naver.com>
2024-02-14 22:52:39 +0900

    도로 네트워크 공부 추가" "
    Signed-off-by: MartinNoh <ehdrud1129@naver.com>

--- src/알고리즘/최소공통조상/boj_11438_LCA_2/Main4.java
+++ src/알고리즘/최소공통조상/boj_11438_LCA_2/Main4.java
@@ -11,9 +11,10 @@ import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main4 {
 	static int N, M;
 	static List<Integer>[] adjList;
+	
 	static int kMax;
 	static int[][] parent;
 	static int[] depth;
@@ -50,31 +51,31 @@ public class Main {
 	}
 	
 	static int lca(int a, int b) {
-		if (depth[a] < depth[b]) {
-			int temp = a;
-			a = b;
-			b = temp;
-		}
-		
-		for (int k = kMax; k >= 0; k--) {
-			if (Math.pow(2, k) <= depth[a] - depth[b]) {
-				a = parent[k][a];
-			}
-		}
-		
-		if (a == b) {
-			return a;
-		}
-		
-		for (int k = kMax; k >= 0; k--) {
-			if (parent[k][a] != parent[k][b]) {
-				a = parent[k][a];
-				b = parent[k][b];
-			}
-		}
-		
-		return parent[0][a];
-	}
+        if (depth[a] < depth[b]) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        for (int k = kMax; k >= 0; k--) {
+            if (Math.pow(2, k) <= depth[a] - depth[b]) {
+                a = parent[k][a];
+            }
+        }
+
+        if (a == b) {
+            return a;
+        }
+
+        for (int k = kMax; k >= 0; k--) {
+            if (parent[k][a] != parent[k][b]) {
+                a = parent[k][a];
+                b = parent[k][b];
+            }
+        }
+
+        return parent[0][a];
+    }
 	
 	public static void main(String[] args) {
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -83,9 +84,10 @@ public class Main {
 			StringBuilder sb = new StringBuilder();
 			
 			N = Integer.parseInt(br.readLine());
+			
 			adjList = new ArrayList[N + 1];
 			for (int n = 1; n <= N; n++) {
-				adjList[n] = new ArrayList<Integer>();
+				adjList[n] = new ArrayList<>();
 			}
 			
 			int u, v;
@@ -98,25 +100,26 @@ public class Main {
 				adjList[v].add(u);
 			}
 			
-			kMax = (int) (Math.log(N) / Math.log(2)) + 1;
+			kMax = (int)(Math.log(N) / Math.log(2)) + 1;
+			
 			parent = new int[kMax + 1][N + 1];
 			depth = new int[N + 1];
 			bfs(1);
 			for (int k = 1; k <= kMax; k++) {
 				for (int n = 1; n <= N; n++) {
-					parent[k][n] = parent[k - 1][parent[k - 1][n]];
+					parent[k][n] = parent[k - 1][parent[k-1][n]];
 				}
 			}
 			
 			M = Integer.parseInt(br.readLine());
-			int a, b, lca;
+			int a, b, result;
 			for (int m = 0; m < M; m++) {
 				st = new StringTokenizer(br.readLine());
 				a = Integer.parseInt(st.nextToken());
 				b = Integer.parseInt(st.nextToken());
 				
-				lca = lca(a, b);
-				sb.append(lca).append("\n");
+				result = lca(a, b);
+				sb.append(result).append("\n");
 			}
 			
 			bw.write(sb.toString().trim());
