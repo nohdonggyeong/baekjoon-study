f5479b157c71b760287efa2e8f4e520a62809b74
노동경 <dandy.noh@samsung.com>
2024-04-08 19:12:35 +0900

    Add union find study
    Signed-off-by: 노동경 <dandy.noh@samsung.com>

--- src/backjoon/알고리즘_분류/유니온파인드/boj_10216_count_circle_groups/Main.java
+++ src/backjoon/알고리즘_분류/유니온파인드/boj_10216_count_circle_groups/Main.java
@@ -5,7 +5,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
@@ -60,35 +59,42 @@ public class Main {
 			StringBuilder sb = new StringBuilder();
 			
 			T = Integer.parseInt(br.readLine());
-			for (int t = 0; t < T; t++) {
-				
+			while (T-- > 0) {
 				N = Integer.parseInt(br.readLine());
 				nodes = new Node[N];
 				parent = new int[N];
 				rank = new int[N];
 				
-				for (int n = 0; n < N; n++) {
+				for (int i = 0; i < N; i++) {
 					st = new StringTokenizer(br.readLine());
 					x = Integer.parseInt(st.nextToken());
 					y = Integer.parseInt(st.nextToken());
 					R = Integer.parseInt(st.nextToken());
 					
-					nodes[n] = new Node(x, y, R);
-					parent[n] = n;
-					rank[n] = 0;
+					nodes[i] = new Node(x, y, R);
+					parent[i] = i;
+					rank[i] = 0;
 				}
 				
-				for (int i = 0; i < N; i++) {
-					for (int j = 0; j < N; j++) {
-						if (Math.pow(nodes[i].x - nodes[j].x, 2) + Math.pow(nodes[i].y - nodes[j].y, 2) <= Math.pow(nodes[i].R + nodes[j].R, 2)) {
+				Node nodeI, nodeJ;
+				for (int i = 0; i < N - 1; i++) {
+					for (int j = i + 1; j < N; j++) {
+						nodeI = nodes[i];
+						nodeJ = nodes[j];
+
+						if (find(i) == find(j)) {
+							continue;
+						}
+						
+						if (Math.pow(nodeI.x - nodeJ.x, 2) + Math.pow(nodeI.y - nodeJ.y, 2) <= Math.pow(nodeI.R + nodeJ.R, 2)) {
 							union(i, j);
 						}
 					}
 				}
 				
 				parentSet = new HashSet<>();
-				for (int i = 0; i < N; i++) {
-					parentSet.add(parent[i]);
+				for (int num : parent) {
+					parentSet.add(find(num));
 				}
 				
 				sb.append(parentSet.size()).append("\n");
